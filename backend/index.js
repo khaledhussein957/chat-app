9877ac12ab51324ea40f96be342a5957ab354407
khaled29-coder <khaledhussein957@gmail.com>
2025-01-27 12:23:37 +0300

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import { app, server } from "./socket/socket.js";
@@ -15,6 +16,13 @@ import messageRoutes from "./routes/messageRoutes.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
 
 app.use(express.json());
 app.use(cookieParser());
